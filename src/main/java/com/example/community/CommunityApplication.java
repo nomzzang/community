3f3e5f1732875bb44cfa0ae8bb49fa49ebe091d0
kyu <starnomzzang@naver.com>
2023-05-18 23:01:50 +0900

    member branch

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityApplication {
-// 멤버 브런치
+// member branch
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
 
